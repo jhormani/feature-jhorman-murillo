d35a8c1f91d14c26822421a8842b4c6d80bd9252
miguel angel <miguelangelcopetegrueso4@gmail.com>
2026-05-18 23:24:43 -0500

    Update index.js

--- mi-webapp/index.js
+++ mi-webapp/index.js
@@ -1,25 +1,27 @@
 const express = require("express");
+const cors = require("cors"); // Agregado para que React se pueda conectar sin bloqueos
 
 const app = express();
-
 const PORT = 3000;
 
+// Middlewares
+app.use(cors()); // Agregado
 app.use(express.json());
+app.use('/imagenes', express.static('public')); // Agregado para servir las fotos de tus productos
 
 /*
 =====================================
 IMPORTAR RUTAS
 =====================================
 */
-
 const productsRoutes = require("./routes/products.routes");
+const cartRoutes = require("./routes/cart.routes"); // Agregado
 
 /*
 =====================================
 RUTA PRINCIPAL
 =====================================
 */
-
 app.get("/", (req, res) => {
   res.json({
     mensaje: "API REST TechStore funcionando"
@@ -31,8 +33,8 @@ app.get("/", (req, res) => {
 USAR RUTAS
 =====================================
 */
-
 app.use("/products", productsRoutes);
+app.use("/cart", cartRoutes); // Agregado
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
